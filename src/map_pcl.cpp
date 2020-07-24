83e05d15ecc65669b2d8bb681181caa45f271715
TadielloM <tadistyle@gmail.com>
2020-07-25 00:11:41 +0200

    written not compiling

---
+++ src/map_pcl.cpp
@@ -0,0 +1,56 @@
+#include <ros/ros.h>
+#include <pcl/point_cloud.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
+
+ros::Publisher pub;
+pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+
+
+void velodyne_callback(const sensor_msgs::PointCloud2 &msg)
+{
+    // Container for original & filtered data
+    pcl::PCLPointCloud2* tmp_cloud = new pcl::PCLPointCloud2; 
+    pcl::PCLPointCloud2ConstPtr tmp_cloudPtr(tmp_cloud);
+    // Convert to PCL data type
+    pcl_conversions::toPCL(msg, *tmp_cloud);
+    
+    *cloud += *tmp_cloud;
+    
+}
+
+void filter_map(){
+    // Create the filtering object
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud (cloud);
+    sor.setLeafSize (0.01, 0.01, 0.01);
+    sor.filter (*cloud_filtered);
+}
+
+int main(int argc, char** argv){
+    //Initialize ROS
+    ros::init (argc,argv,"map_pcl");
+    ros::NodeHandle nh;
+    ros::Subscriber velodyne_subscriber;
+    velodyne_subscriber = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 10, velodyne_callback);
+    pub = nh.advertise<sensor_msgs::PointCloud2> ("/map_points");
+    ros::Rate(1);
+
+    while(ros::ok()){
+        pcl::PCLPointCloud2* cloud_filtered;
+        // Create the filtering object and perform filtering
+        pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+        sor.setInputCloud (cloudPtr);
+        sor.setLeafSize (0.5, 0.5, 0.5); //50cm filter
+        sor.filter (cloud_filtered);
+
+        sensor_msgs::PointCloud2 output;
+        pcl_conversions::fromPCL(cloud_filtered, output);
+
+        pub.publish();
+        ros::spinOnce();
+    }
+    return 0;
+}
