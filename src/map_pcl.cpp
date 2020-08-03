0a8879d1d358ab00e9d5b074028e829c142c3c6c
TadielloM <tadistyle@gmail.com>
2020-08-03 15:28:03 +0200

    transformed pointcloude before adding them

--- src/map_pcl.cpp
+++ src/map_pcl.cpp
@@ -5,11 +5,15 @@
 #include <pcl/point_types.h>
 #include <pcl/filters/voxel_grid.h>
 #include <std_srvs/Empty.h>
+#include <tf/transform_listener.h>
+#include <pcl_ros/transforms.h>
 
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+tf::TransformListener* listener;
 
 void velodyne_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
 {
+    // ROS_INFO("Point Cloud Map: callback");
     //Converting ROSmsg to pPCL
     pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg (*msg, *tmp_cloud);
@@ -19,7 +23,10 @@ void velodyne_callback(const sensor_msgs::PointCloud2ConstPtr &msg)
     sor.setLeafSize (0.5, 0.5, 0.5); //50cm filter
     sor.filter (*tmp_cloud);
     //Add new input to previus cloud
-    *cloud = *cloud + *tmp_cloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr tmp_cloud2 (new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl_ros::transformPointCloud("map", *tmp_cloud, *tmp_cloud2, *listener);
+    *cloud = *cloud + *tmp_cloud2;
 }
 
 bool resetSrv(std_srvs::Empty::Request& req, std_srvs::Empty::Response& resp) {
@@ -32,6 +39,8 @@ int main(int argc, char** argv){
     //Initialize ROS
     ros::init (argc,argv,"map_pcl");
     ros::NodeHandle nh;
+    
+    listener = new(tf::TransformListener);
     //Set up subscriber and Publisher
     ros::Subscriber velodyne_subscriber;
     velodyne_subscriber = nh.subscribe<sensor_msgs::PointCloud2>("/velodyne_points", 10, velodyne_callback);
@@ -41,6 +50,16 @@ int main(int argc, char** argv){
     ros::Rate rate(1);
 
     while(ros::ok()){
+        //listen tf transform
+        tf::StampedTransform transform;
+        try{
+          listener->lookupTransform("/velodyne_base", "/map",  
+                                   ros::Time(0), transform);
+        }
+        catch (tf::TransformException ex){
+          ROS_ERROR("%s",ex.what());
+          ros::Duration(1.0).sleep();
+        }
         // Create the filtering object and perform filtering
         pcl::VoxelGrid<pcl::PointXYZ> sor;
         sor.setInputCloud (cloud);
@@ -52,6 +71,8 @@ int main(int argc, char** argv){
         pcl::PCLPointCloud2Ptr cloud_tmp(new pcl::PCLPointCloud2());
         pcl::toPCLPointCloud2(*cloud, *cloud_tmp);
         pcl_conversions::fromPCL(*cloud_tmp, output);
+
+        output.header.frame_id = "map";
 
         pub.publish(output);
         rate.sleep();
